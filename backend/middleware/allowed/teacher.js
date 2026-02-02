29125de17047b805d625089a705134616e6515c9
aditya <adityabibhas.sahu@gmail.com>
2026-02-02 22:36:44 +0530

    Fixed env var

--- backend/middleware/allowed/teacher.js
+++ backend/middleware/allowed/teacher.js
@@ -2,7 +2,7 @@ const {UnauthenticatedError}= require('../../utils/errors');
 
 // course and enroll endpoint middleware to check if user is teacher
 const teacherAllowedMiddleware=(req,res,next)=>{
-    if(req.user.role !== 'teacher'){
+    if(req.user.role !== 'Teacher'){
         throw new UnauthenticatedError('Not authorized to access this route');
     }
     next();
--- backend/middleware/authentication.js
+++ backend/middleware/authentication.js
@@ -5,10 +5,11 @@ const authenticationMiddleware=(req,res,next)=>{
     const authHeader= req.headers.authorization;
     if(!authHeader || !authHeader.startsWith('Bearer ')) throw new UnauthenticatedError('Token not found');
     const token =authHeader.split(' ')[1];
-
+    // console.log(token);
     try{
         const payload = jwt.verify(token, process.env.JWT_SECRET)
         req.user = { id: payload.id, name: payload.name, email: payload.email, role: payload.role }
+        console.log(req.user);
         next();
     }
     catch(error){
